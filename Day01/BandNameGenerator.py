183175eec623dc58ee9dd959e37cde4e3dea11b0
Charlie Mathot <charliemathot@MacBook-Air-de-Charlie.local>
2025-12-09 14:58:10 +0100

    First exercice completed

---
+++ Day01/BandNameGenerator.py
@@ -0,0 +1,6 @@
+#Small program generating a band name based on your city and first pet name
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in ?\n")
+petName = input ("What's the name of your first pet\n")
+bandName = city + " " + petName
+print("Your band name could be " + bandName)
